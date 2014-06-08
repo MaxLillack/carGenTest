01712f1f736246db8619c6fef3d5c5506e963e70
Ingo-Rößner <ingo.spicker@gmail.com>
2014-06-08 04:00:38 +0200

    add border tests

--- main.cpp
+++ main.cpp
@@ -7,10 +7,26 @@ using namespace std;
 
 int main()
 {
-    LTest::addTest("test 1", [](){
+    LTest::addTest("containsPart() inBound", [](){
         Config<parts::Body, parts::Engine> conf;
         LTAssert::True(conf.containsPart<parts::Body>());
         LTAssert::True(conf.containsPart<parts::Engine>());
+        LTAssert::False(conf.containsPart<parts::Transmission>());
+        return true;
+    });
+
+    LTest::addTest("containsPart() Border (one part)", [](){
+        Config<parts::Body> conf;
+        LTAssert::True(conf.containsPart<parts::Body>());
+        LTAssert::False(conf.containsPart<parts::Engine>());
+        LTAssert::False(conf.containsPart<parts::Transmission>());
+        return true;
+    });
+
+    LTest::addTest("containsPart() Border (zero parts)", [](){
+        Config<> conf;
+        LTAssert::False(conf.containsPart<parts::Body>());
+        LTAssert::False(conf.containsPart<parts::Engine>());
         LTAssert::False(conf.containsPart<parts::Transmission>());
         return true;
     });
