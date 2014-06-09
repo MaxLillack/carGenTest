27914508a953f0bd07277918afca7872f7b91839
Ingo-Rößner <ingo.spicker@gmail.com>
2014-06-09 12:09:26 +0200

    change test syntax & add containsDerivedPart() tests

--- main.cpp
+++ main.cpp
@@ -8,26 +8,45 @@ using namespace std;
 int main()
 {
     LTest::addTest("containsPart() inBound", [](){
-        Config<parts::Body, parts::Engine> conf;
-        LTAssert::True(conf.containsPart<parts::Body>());
-        LTAssert::True(conf.containsPart<parts::Engine>());
-        LTAssert::False(conf.containsPart<parts::Transmission>());
+        using Conf = Config<parts::Body, parts::GasEngine>;
+        LTAssert::True(Conf::containsPart<parts::Body>());
+        LTAssert::True(Conf::containsPart<parts::GasEngine>());
+        LTAssert::False(Conf::containsPart<parts::AutomaticTransmission<5>>());
         return true;
     });
 
     LTest::addTest("containsPart() Border (one part)", [](){
-        Config<parts::Body> conf;
-        LTAssert::True(conf.containsPart<parts::Body>());
-        LTAssert::False(conf.containsPart<parts::Engine>());
-        LTAssert::False(conf.containsPart<parts::Transmission>());
+        using Conf = Config<parts::Body>;
+        LTAssert::True(Conf::containsPart<parts::Body>());
+        LTAssert::False(Conf::containsPart<parts::GasEngine>());
+        LTAssert::False(Conf::containsPart<parts::AutomaticTransmission<5>>());
         return true;
     });
 
     LTest::addTest("containsPart() Border (zero parts)", [](){
-        Config<> conf;
-        LTAssert::False(conf.containsPart<parts::Body>());
-        LTAssert::False(conf.containsPart<parts::Engine>());
-        LTAssert::False(conf.containsPart<parts::Transmission>());
+        using Conf = Config<>;
+        LTAssert::False(Conf::containsPart<parts::Body>());
+        LTAssert::False(Conf::containsPart<parts::GasEngine>());
+        LTAssert::False(Conf::containsPart<parts::AutomaticTransmission<5>>());
+        return true;
+    });
+
+
+    //containsDerivedPart Tests
+    LTest::addTest("containsDerivedPart() 1", [](){
+        using Conf = Config<parts::AutomaticTransmission<5>, parts::GasEngine>;
+        LTAssert::False(Conf::containsDerivedPart<parts::Body>());
+        LTAssert::True(Conf::containsDerivedPart<parts::GasEngine>());
+        LTAssert::True(Conf::containsDerivedPart<parts::Transmission>());
+        return true;
+    });
+
+    LTest::addTest("containsDerivedPart() 2", [](){
+        using Conf = Config<parts::AutomaticTransmission<5>, parts::GasEngine>;
+        LTAssert::False(Conf::containsDerivedPart<parts::Body>());
+        LTAssert::True(Conf::containsDerivedPart<parts::GasEngine>());
+        LTAssert::True(Conf::containsDerivedPart<parts::AutomaticTransmissionBase>());
+        LTAssert::False(Conf::containsDerivedPart<parts::ManualTransmissionBase>());
         return true;
     });
 
